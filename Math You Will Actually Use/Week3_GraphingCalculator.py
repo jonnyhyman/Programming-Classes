af058e8e3f60a0cc1c15ceb109ab7acbc37b068b
jonnyhyman <jonnyhyman@gmail.com>
2018-02-15 16:43:32 -0800

    Added week 3

---
+++ Math You Will Actually Use/Week3_GraphingCalculator.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+
+userTyped = input('Type your equation here! : ')
+
+x = range(0, 100)
+y = [some_list_with stuff]
+
+plt.plot(x, y)
+
+four = eval('2**2') # will return 4
