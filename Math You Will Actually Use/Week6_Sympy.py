28c0d7efbe767e503e2de108e9a0073223851d01
jonnyhyman <jonnyhyman@gmail.com>
2018-03-08 19:32:50 -0800

    Updated sympy week6

--- Math You Will Actually Use/Week6_Sympy.py
+++ Math You Will Actually Use/Week6_Sympy.py
@@ -1,17 +1,24 @@
+' "pip install sympy" before you do any of this '
 
+import sympy  # import the calculator with variable buttons :)
 
-' "pip install sympy" before you do this '
-import sympy
+# -------------------------- first, solve the simple equation 2*x=2
+x = sympy.Symbol('x')  # create a "symoblic variable" x
 
-x = sympy.Symbol('x')
+equation = 2*x - 2  # define the equation 2*x = 2
 
-eq = 2*x - 2
-sol=sympy.solve(eq)  # solve for x
-print('congratulations:',sol)
+solution = sympy.solve(equation, x)  # solve for x
 
+print(solution)  # print the list of all solutions
+
+# -------------------------- next, lets do the equation of a circle
 y = sympy.Symbol('y')
 
-# this is equig
-eq = x**2 + y**2 - 1
-sol=sympy.solve(eq)  # solve for x
-print('congratulations:',sol)
+equation = x**2 + y**2 - 1**2 # x**2 + y**2 = r**2 where r =1
+
+solution = sympy.solve(equation, y)  # solve for y
+
+print(solution)
+
+# notice how one is the TOP half of the circle
+# and the other in the list is the BOTTOM half
