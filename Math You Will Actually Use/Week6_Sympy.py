be58c83158150c7b36f760f4e66afbd8b6c6e682
jonnyhyman <jonnyhyman@gmail.com>
2018-03-08 19:33:42 -0800

    cleanup cleanup

--- Math You Will Actually Use/Week6_Sympy.py
+++ Math You Will Actually Use/Week6_Sympy.py
@@ -1,9 +1,10 @@
 ' "pip install sympy" before you do any of this '
 
 import sympy  # import the calculator with variable buttons :)
+from sympy import Symbol
 
 # -------------------------- first, solve the simple equation 2*x=2
-x = sympy.Symbol('x')  # create a "symoblic variable" x
+x = Symbol('x')  # create a "symoblic variable" x
 
 equation = 2*x - 2  # define the equation 2*x = 2
 
@@ -12,7 +13,7 @@ solution = sympy.solve(equation, x)  # solve for x
 print(solution)  # print the list of all solutions
 
 # -------------------------- next, lets do the equation of a circle
-y = sympy.Symbol('y')
+y = Symbol('y')
 
 equation = x**2 + y**2 - 1**2 # x**2 + y**2 = r**2 where r =1
 
