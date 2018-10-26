1a76cb1fa8c2786e058f4e23c1b6d1cba566cc5b
Jonny Hyman <jonnyhyman@gmail.com>
2018-10-26 13:14:41 -0700

    Updates for Week 8 of Math Class

--- Math Magic/Week8_PringlePlotter3000.py
+++ Math Magic/Week8_PringlePlotter3000.py
@@ -9,17 +9,15 @@
         THE
         PRINGLLLLLLEEEEEEEEEEEEEEEEEEEEE
 
-        Challenge 1:
+        Challenge 1 TABASCO SPICY:
             Try to figure out how to make the PRINGLLLLLLEEEEEEEEEEEEEEEEEEEEE
-            niiiiiicee and smoooooth, not some rectange piece of TRASH
+            niiiiiicee and smoooooth, not some rectangle piece of TRASH
 
-        Challenge 2:
+        Challenge 2 HABANERO SPICY:
             Try to figure out how to change the color of it
 
-        Challenge 3:
-            Try to figure out how to make it CIRCULAR (from above)
-            instead of some kind of rectangular piece of TRASH
-                (these aren't pita chips, after all...)
+        Challenge 3 GHOST PEPPER SPICYNESS:
+            Give it a completely different shape, such as a chipotle bowl?!?!
 
 '''
 
@@ -29,8 +27,6 @@
 from mpl_toolkits.mplot3d import Axes3D  # import the 3D thingy from matplotlib
 import matplotlib.pyplot as plt          # import the plotter from matplotlib
 from matplotlib import cm                # import matplotlib itself :O
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-# I have no clue what the above line does...
 
 import numpy as np  # import numpy, a really useful math / matrix package
 
@@ -39,22 +35,9 @@ fig = plt.figure()  # create a plot "figure" (window holding our PRINGLLLLLLEEEE
 ax = fig.gca(projection='3d')  # tell the plot to be 3 dimensional
 
 # Make data.
-X = np.arange(-1, 1, 0.1)  # from -5 to 5, at every .25 along the way
-Y = np.arange(-1, 1, 0.1)  # from -5 to 5, at every .25 along the way
+X = np.arange(-1, 1, 0.25)  # from -1 to 1, at every .25 along the way
+Y = np.arange(-1, 1, 0.25)  # from -1 to 1, at every .25 along the way
 X, Y = np.meshgrid(X, Y)    # for every x, make a line of ys and vice-a-versa
-
-make_circle_pringle = False  # should we delete points outside of an XY circle?
-
-if make_circle_pringle:
-
-    # Notice that the new surface when we do this is extremely jagged
-    # Super ugly actually.... Why is this?
-
-    X = np.where(X**2 + Y**2 <= 1, X, 0)
-    Y = np.where(X**2 + Y**2 <= 1, Y, 0)
-
-    # Because each time we get to one side, we teleport to the other side
-    # and accidentally "drag the pen" along the way
 
 # Now, for every single X, Y pair, create a 3rd list, Z, which has the value
 # for Z of that X,Y pair. If (X,Y) = (2,3), then Z = (2**2 + 3**2) there!
@@ -73,8 +56,6 @@ surf = ax.plot_surface( X, Y, Z,
 
 # Customize the z axis.
 ax.set_zlim(-1.01, 1.01)
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
 # Add a color bar which maps values to colors.
 fig.colorbar(surf, shrink=0.5, aspect=5)
