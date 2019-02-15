7c9407439af07d44d19b4d8439579cc92c82f42d
Jonny Hyman <jonnyhyman@gmail.com>
2019-02-15 10:08:05 -0800

    Week 3 ksp

--- Python Projects/AUDIOSIMULTANEOUS.py
+++ Python Projects/AUDIOSIMULTANEOUS.py
@@ -1,16 +1,23 @@
 import PyAudioMixer as pam
-import time
-
+from time import time
 import wave
 
 #import pdb; pdb.set_trace()
 
 mixer = pam.Mixer()
-
-snd = pam.Sound(mixer, filename="Cs5.wav")
-snd1 = pam.Sound(mixer, filename="Cs5.wav")
-
 mixer.start()
+snd = pam.Sound(mixer, filename="Cs5.wav")
+snd1 = pam.Sound(mixer, filename="Gs5.wav")
+
+
 snd.play()
-#time.sleep(0.5)
-snd1.play()
+
+notstarted = True
+start = time()
+while True:
+
+    if time()-start > 1 and notstarted:
+        print('STARTED1')
+        snd1.play()
+        print('STARTED2')
+        notstarted = False
